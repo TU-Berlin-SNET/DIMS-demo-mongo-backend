156cc5d0c865e0e573eb592de2d3ffe86da8ac70
Ömer Ilhan <malik@mailbox.tu-berlin.de>
2019-08-27 10:36:14 +0200

    add 404 handler

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const config = require('./config')
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-const authmiddleware = require('./auth-middleware')
+const authMiddleware = require('./auth-middleware')
 const routes = require('./routes')
 
 const app = express()
 
 app.use(morgan('combined'))
+app.use(authMiddleware)
 app.use(bodyParser.json())
-app.use(authmiddleware)
 app.use('/', routes)
+app.use((req, res, next) => res.status(404).json({ message: 'nothing here' }))
 
 app.listen(config.APP_PORT, config.APP_HOST, (server) => console.log('demo backend running at %s:%s', config.APP_HOST, config.APP_PORT))
