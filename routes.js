6fe77da60465a5c6c6da73f07a6d52c7bd37e0ca
Marcel Ebermann <marcel.ebermann@campus.tu-berlin.de>
2019-10-22 18:34:41 +0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -17,7 +17,7 @@ const routes = [
   {
     name: 'legal-person',
     model: require('./models/legal-person'),
-    roles: ['government', 'shop']
+    roles: ['government']
   },
   {
     name: 'natural-person',
